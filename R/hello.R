83716fab876a84a2d01a79caa4d14f0df82ce5f5
Wojciech Francuzik <wolass@gmail.com>
2018-04-19 17:55:43 +0200

    function ready

--- R/hello.R
+++ R/hello.R
@@ -1,34 +1,32 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+#' @title Timestamp a file
+#' @description This function uses cryptography to timestamp a text file
+#' @param filename This is the text file that will be read into the function using ReadLines. After this the hash will be produced
+#' @param key This is the authentication token used on your stamping site if you are using originstam.org then get it here http://originstamp.org/dev
+#' @param service Right now only originstamp is supported.
+#' @export
 
 require("magrittr")
 require("digest")
 require(httr)
 
-timestamp <- function(filename,key,service = "originstamp") {
-  body <- list(hash_sha256 = readLines(filename) %>%
-                 digest::digest(algo="sha256"))
+
+timestamp <- function(filename,
+                      key,
+                      service = "originstamp",
+                      email,
+                      comment) {
+  body <- list(email=email,
+               comment=comment
+  )
   if(service=="originstamp"){
     base <- "http://api.originstamp.org/api/"
     endpoint <- "stamps"
-    url <- paste0(base,endpoint)
+    url <- paste0(base,readLines(filename) %>%
+                    digest::digest(algo="sha256"))
     obj <- httr::POST(url = url,
                       body = body,
-                      add_headers(Authorization = paste0("Token token='",key,"'")),
+                      add_headers(Authorization = paste0(key)),
                       encode = "json",
                       verbose())
   }
 }
-
